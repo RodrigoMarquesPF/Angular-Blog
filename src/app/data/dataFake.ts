8854a979d8fd41cebbb7f55af10e22430d66be62
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-01-26 00:21:34 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    id: '1',
+
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci nemo totam facere fuga distinctio maiores necessitatibus eaque, cum assumenda velit delectus! A, voluptates? Tempore beatae id neque perferendis aperiam impedit',
+    photoCover:
+      'https://i.ytimg.com/vi/SSCLPoNofNQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBY93-ro4WH-NkLyMCBhEeJGn9onA',
+  },
+  {
+    id: '2',
+
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'dfndjnfiehwifenjkfnewjkfnwejkf',
+    photoCover: 'https://i.ytimg.com/vi/IRmMYiQi1b0/sddefault.jpg',
+  },
+  {
+    id: '3',
+
+    title: 'Lorem ipsum dolor sit amet.',
+    description: 'dfndjnfiehwifenjkfnewjkfnwejkf',
+    photoCover: 'https://lh5.googleusercontent.com/rsShK9L_i9BZqu_4yP0rc8TRRFeKsQhPT9vJxs47HcvCO1ca8TIW3FSWcAySaeYsKnhcWQ4WKI-07RhgiuzqO4UwOwzAX4ojiShgMSmVAHaBSrVJNlBQUMqhl4HpoAOd5MILCNuYmIjlmRglpo5q2Jk',
+  },
+  {
+    id: '4',
+
+    title: 'Lorem ipsum dolor si elit.',
+    description: 'dfndjnfiehwifenjkfnewjkfnwejkf',
+    photoCover: 'https://i.redd.it/4alvivduty881.jpg',
+  },
+];
